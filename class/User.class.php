29e9e27b655837152ea697f00139b0184d02065b
sajmonn1 <szym00noff@gmail.com>
2024-05-20 13:09:17 +0200

    poprawka

--- class/User.class.php
+++ class/User.class.php
@@ -1,97 +1,131 @@
 <?php
 class User {
-    //właściwości klasy User czyli "co użytkownik MA"
     private $id;
-    private $email;
+    private $login;
     private $password;
 
-    //metody klasy User czyli "co użytkownik ROBI"
-
-    //konstruktor
-    public function __construct(int $id, string $email)
-    {
-        //this oznacza tworzony właśnie obiekt lub instancję klasy do której się odnosimy
+    public function __construct(int $id, string $login, string $password) {
         $this->id = $id;
-        $this->email = $email;
-    }
-    //getery:
-    public function getEmail() : string {
-        return $this->email;
+        $this->login = $login;
+        $this->password = $password;
     }
 
+    public static function Register(string $login, string $password) : bool {
+        $db = new mysqli("localhost", "root", "", "cms");
+        if ($db->connect_errno) {
+            echo "Failed to connect to MySQL: ". $db->connect_error;
+            exit();
+        }
 
-    public static function Register(string $email, string $password) : bool {
-        //funkcja rejestruje nowego użytkownika do bazy danych....
-        //funkcja zwraca true jeśli się udało lub false jeśli się nie udało
-        $db = new mysqli('localhost', 'root', '', 'cms');
-        $sql = "INSERT INTO user (email, password) VALUES (?, ?)";
+        $sql = "INSERT INTO user (login, password) VALUES (?,?)";
         $q = $db->prepare($sql);
+        if (!$q) {
+            echo "Error preparing query: ". $db->error;
+            exit();
+        }
+
         $passwordHash = password_hash($password, PASSWORD_ARGON2I);
-        $q->bind_param("ss", $email, $passwordHash);
+        $q->bind_param("ss", $login, $passwordHash);
         $result = $q->execute();
+
+        if (!$result) {
+            echo "Error executing query: ". $db->error;
+            exit();
+        }
+
+        $db->close();
         return $result;
     }
-    public static function Login(string $email, string $password) : bool {
-        //funkcja loguje istniejacego uzytkownika do bazy danych...
-        //funkcja zapisuje użytkownika do sesji i zwraca true jeśli użytkownik istnieje
-        //funkcja zwraca false jeśli użytkownik o takim haśle nie istnieje
-        $db = new mysqli('localhost', 'root', '', 'cms');
-        $sql = "SELECT * FROM user WHERE email = ? LIMIT 1";
+
+    public static function Login(string $login, string $password) : bool {
+        $db = new mysqli("localhost", "root", "", "cms");
+        if ($db->connect_errno) {
+            echo "Failed to connect to MySQL: ". $db->connect_error;
+            exit();
+        }
+
+        $sql = "SELECT password FROM user WHERE login =?";
         $q = $db->prepare($sql);
-        $q->bind_param("s", $email);
+        if (!$q) {
+            echo "Error preparing query: ". $db->error;
+            exit();
+        }
+
+        $q->bind_param("s", $login);
         $q->execute();
         $result = $q->get_result();
         $row = $result->fetch_assoc();
-        //tu muszą się nazwy w nawiasach [] zgadzać z nazwą kolumny w bazie danych
-        $id = $row['ID'];
-        $passwordHash = $row['password'];
-        if(password_verify($password, $passwordHash)) {
-            //hasło się zgadza
-            //zapisz dane użytkownika do sesji
-            $user = new User($id, $email);
-            $_SESSION['user'] = $user;
-            return true;
-        } else {
-            //hasło się nie zgadza
+
+        if ($row === null) {
             return false;
         }
+
+        if (password_verify($password, $row['password'])) {
+            return true;
+        } else {
+            return false;
+        }
+
+        $db->close();
     }
+
+    public function Logout() {
+        session_destroy();
+    }
+
+    public function ChangePassword(string $oldLogin, string $newPassword) : bool {
+        $db = new mysqli("localhost", "root", "", "cms");
+        if ($db->connect_errno) {
+            echo "Failed to connect to MySQL: ". $db->connect_error;
+            exit();
+        }
+
+        $sql = "SELECT password FROM user WHERE login =?";
+        $q = $db->prepare($sql);
+        if (!$q) {
+            echo "Error preparing query: ". $db->error;
+            exit();
+        }
+
+        $q->bind_param("s", $this->login);
+        $q->execute();
+        $result = $q->get_result();
+        $row = $result->fetch_assoc();
+
+        if ($row === null) {
+            return false;
+        }
+
+        if (password_verify($oldLogin, $row['password'])) {
+            $newPasswordHash = password_hash($newPassword, PASSWORD_ARGON2I);
+            $sql = "UPDATE user SET password =? WHERE login =?";
+            $q = $db->prepare($sql);
+            if (!$q) {
+                echo "Error preparing query: ". $db->error;
+                exit();
+            }
+
+            $q->bind_param("si", $newPasswordHash, $this->login);
+            $result = $q->execute();
+
+            if (!$result) {
+                echo "Error executing query: ". $db->error;
+                exit();
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+
+        $db->close();
+    }
+
     public static function isLogged() {
         if(isset($_SESSION['user']))
             return true;
         else 
             return false;
     }
-    public function Logout() {
-        //funkcja wylogowuje użytkownika
-        session_destroy();
-    }
-    public function ChangePassword(string $oldPassword, string $newPassword) : bool  {
-        //ta funkcja ma zaktualizować hasło użytkownika w bazie danych
-        //wyciągnij hash hasła z bazy danych
-        $db = new mysqli("localhost", "root", "", "cms");
-        $sql = "SELECT password FROM USER WHERE user.ID = ?";
-        $q = $db->prepare($sql);
-        $q->bind_param("i", $this->id);
-        $q->execute();
-        //$result to jest mysqli_result
-        $result = $q->get_result();
-        $row = $result->fetch_assoc();
-        $oldPasswordHash = $row['password'];
-
-        if(password_verify($oldPassword, $oldPasswordHash)){
-            //użytkownik wprowadził poprawne _stare_ hasło
-            $newPasswordHash = password_hash($newPassword, PASSWORD_ARGON2I);
-            $sql = "UPDATE user SET password = ? WHERE user.ID = ?";
-            $q = $db->prepare($sql);
-            $q->bind_param("si", $newPasswordHash, $this->id);
-            $result = $q->execute();
-            return $result;
-        } else {
-            return false;
-        }
-    }
-    
 }
-
 ?>
